6df8ad1f14bd932a30f061f924d544bfe922a5b2
AHKnoesen <127387880+AHKnoesen@users.noreply.github.com>
2023-05-04 15:02:15 +0200

    Add files via upload

---
+++ Projects/interactive-web-apps/IWA_2/IWA_2.4.js
@@ -0,0 +1,8 @@
+// This is the firstname of the user
+const user = 'John';
+
+// This is the lastname of the user
+const surname = 'Smith';
+
+// Log the full name of the user
+console.log(`${user} ${surname}`);
---
+++ Projects/interactive-web-apps/IWA_3/IWA_3.3/configuration.js
@@ -0,0 +1,3 @@
+// configuration.js
+export const company = 'ACME Inc.'; // Use 'export' to make variables accessible from other files
+export const year = 2022;

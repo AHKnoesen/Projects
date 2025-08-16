ceb9854e52b28293df101a2101b226262b531a59
AHKnoesen <127387880+AHKnoesen@users.noreply.github.com>
2025-08-16 02:27:25 +0200

    Delete Projects directory

--- Projects/interactive-web-apps/IWA_2/IWA_2.4.js
+++
@@ -1,8 +0,0 @@
-// This is the firstname of the user
-const user = 'John';
-
-// This is the lastname of the user
-const surname = 'Smith';
-
-// Log the full name of the user
-console.log(`${user} ${surname}`);
--- Projects/interactive-web-apps/IWA_3/IWA_3.3/configuration.js
+++
@@ -1,3 +0,0 @@
-// configuration.js
-export const company = 'ACME Inc.'; // Use 'export' to make variables accessible from other files
-export const year = 2022;
